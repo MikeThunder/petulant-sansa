97651ead855056c9372763e0a8cfabd4f8b8bb19
Sean <themorselisstring@users.noreply.github.com>
2015-01-15 12:18:46 +1300

    Onclick events work, select box added
    HTML select box added, clicking on any element draws the table

--- ProtoRR/scripts.js
+++ ProtoRR/scripts.js
@@ -25,7 +25,40 @@ function createTable(inputArray,tableWidth) {
 	}
 };
 
+function listsArrayIndexToTable (inputArray) {
+	"use strict"
+	var mutableArray;
+	mutableArray = inputArray.slice(0);
+	return inputArray.slice(2);
+}
 
+var selectList = function() {
+	// onclick event for list box
+	"use strict";
+	var selectBox;
+	
+	selectBox = document.getElementById("listList");
+	document.getElementById("selectedList").textContent = "Selected list : " + listsArray[selectBox.selectedIndex][1];
+	createTable(listsArray[selectBox.selectedIndex].slice(2),5);
+};
+
+function populateSelectBox (input2DArray) {
+	"use strict"
+	// function takes an input 2d array and populates a listbox
+	// allowing the user to select a list
+	var selectBox, // mapped to DOM object
+		option,
+		count;
+	
+	selectBox = document.getElementById("listList");
+	
+	for ( count = 0; count < input2DArray.length; count = count + 1) {
+		option = document.createElement("option")
+		option.innerHTML = input2DArray[count][0] + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + input2DArray[count][1];
+		//option.text = option.text.replace(" ", "&nbsp;");
+		selectBox.add(option)
+	}
+}
 function parse2DListArray (inputString) {
 	"use strict";
 	// function takes an input string from the file loader and parses
@@ -49,6 +82,7 @@ var loadedHandler = function (event) {
 	console.log("LOADEDHANDLER: Filestring = ");
 	console.log(fileString);
 	parse2DListArray(fileString);
+	populateSelectBox(listsArray);
 	//wordsArray = fileString.split(",");
 	//createTableFor(wordsArray,5);
 };
@@ -65,4 +99,6 @@ var fileChangedHandler = function (event) {
 var initialise = function () {
 	"use strict";
 	document.getElementById("loadFileButton").addEventListener("change", fileChangedHandler);
+	
+	document.getElementById("listList").addEventListener("click", selectList);
 }
